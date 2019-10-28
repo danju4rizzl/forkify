67c250e4428a2cb419d8e5ee8ea109388982dfb4
danju4rizzl <danjumashiwaju@gmail.com>
2019-10-28 21:05:50 +0200

    added loading icon & pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from "./models/Search";
 import * as searchView from "./views/searchView";
-import { elements } from "./views/base";
+import { elements, renderLoader, clearLoader } from "./views/base";
 import config from "./config";
 
 /* Global State of the app
@@ -16,9 +16,14 @@ const controlSearch = async () => {
   const query = searchView.getInput();
   if (query) {
     state.search = new Search(query);
+
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
+
     await state.search.getResults();
+
+    clearLoader();
     searchView.renderResults(state.search.result);
   }
 };
@@ -26,4 +31,14 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener("submit", e => {
   e.preventDefault();
   controlSearch();
+});
+
+elements.searchResPages.addEventListener("click", e => {
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+    console.log(goToPage);
+  }
 });
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,6 +8,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
   elements.searchReasultList.innerHTML = "";
+  elements.searchResPages.innerHTML = "";
 };
 
 const limitRecipeTitle = (title, limit = 17) => {
@@ -44,6 +45,46 @@ const renderRecipe = recipe => {
   elements.searchReasultList.insertAdjacentHTML("beforeend", markup);
 };
 
-export const renderResults = recipes => {
-  recipes.forEach(renderRecipe);
+// Type "Prev" or "Next"
+const createButton = (page, type) => `
+          <button class="btn-inline results__btn--${type}" data-goto=${
+  type === "prev" ? page - 1 : page + 1
+}>
+<span>Page ${type === "prev" ? page - 1 : page + 1}</span>
+              <svg class="search__icon">
+                  <use href="img/icons.svg#icon-triangle-${
+                    type === "prev" ? "left" : "right"
+                  }"></use>
+              </svg>
+          </button>
+`;
+
+const resnderButtons = (page, numResults, resPerPage) => {
+  const pages = Math.ceil(numResults / resPerPage);
+  let button;
+
+  if (page === 1 && pages > 1) {
+    // Only Button to go to next page
+    button = createButton(page, "next");
+  } else if (page < pages) {
+    // Both Buttons
+    button = `${createButton(page, "prev")}`;
+    button = `${createButton(page, "nex")}`;
+  } else if (pages === pages && pages > 1) {
+    // Only Button to go to next page
+    button = createButton(page, "prev");
+  }
+
+  elements.searchResPages.insertAdjacentHTML("afterbegin", button);
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+  // Render res of current page
+  const start = (page - 1) * resPerPage;
+  const end = page * resPerPage;
+
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  // Render pagination buttons
+  resnderButtons(page, recipes.length, resPerPage);
 };
